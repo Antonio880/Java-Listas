7ebb0a2c6475eab767252ae61918e07c8a6fb4a5
AntônioCruz <106624745+Antonio880@users.noreply.github.com>
2023-03-29 13:43:43 -0300

    Add files via upload

--- Lista1AntonioCruz/src/Exerc13.java
+++ Lista1AntonioCruz/src/Exerc13.java
@@ -1,4 +1,18 @@
 
 public class Exerc13 {
-
+	public static void main(String[] args) {
+		int A = 11;
+		int B = 9;
+		
+		if(A > 10) {
+			System.out.println("A > 10");
+		}else {
+			System.out.println("numeros nao validos");
+		}
+		if(A + B== 20) {
+			System.out.println("A + B == 20");
+		}else {
+			System.out.println("numeros nao validos");
+		}
+	}
 }
